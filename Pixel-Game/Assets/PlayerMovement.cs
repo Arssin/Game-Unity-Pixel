2f2cbe34a620e9885b9a30c0a2b8a4a56bca1a85
Mateusz Binięda <mat.binieda@gmail.com>
2023-07-01 21:04:30 +0200

    add player movement

--- Pixel-Game/Assets/PlayerMovement.cs
+++ Pixel-Game/Assets/PlayerMovement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+
     void Update()
     {
-        
+        // Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // Movement
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
     }
 }
