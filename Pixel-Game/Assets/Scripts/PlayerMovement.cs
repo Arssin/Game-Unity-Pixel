a9cf1b2f68ad00fc41d29520c18def73c002279a
Mateusz Binięda <mat.binieda@gmail.com>
2023-07-02 19:22:07 +0200

    player movement update

--- Pixel-Game/Assets/Scripts/PlayerMovement.cs
+++ Pixel-Game/Assets/Scripts/PlayerMovement.cs
@@ -1,27 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    public float moveSpeed = 5f;
-
-    public Rigidbody2D rb;
-
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    private Animator animator;
     private Vector2 movement;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
-        // Input
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Ustalenie wektora kierunku na podstawie wprowadzanych danych
+        movement = new Vector2(moveHorizontal, moveVertical).normalized;
+
+        // Ustalenie animacji zale¿nej od wektora kierunku
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        // Movement
-        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+        // Obracanie postaci w lewo lub prawo
+        if (movement.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (movement.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        // Wykonanie ruchu
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+
     }
 }
